f63dcd3267f1796d62acfc28603f34137eae4195
jurabek <jurabeka@pm.me>
2026-09-08 21:37:45 +0200

    formatting and moving into absolute paths

--- application/src/components/ui/scroll-area.tsx
+++ application/src/components/ui/scroll-area.tsx
@@ -1,8 +1,8 @@
 "use client";
 
-import * as React from "react";
-import { cn } from "@/lib/utils.ts";
 import { ScrollArea as ScrollAreaPrimitive } from "radix-ui";
+import type * as React from "react";
+import { cn } from "@/lib/utils.ts";
 
 function ScrollArea({
 	className,
--- application/src/components/ui/tooltip.tsx
+++ application/src/components/ui/tooltip.tsx
@@ -1,8 +1,8 @@
 "use client";
 
-import * as React from "react";
-import { cn } from "@/lib/utils.ts";
 import { Tooltip as TooltipPrimitive } from "radix-ui";
+import type * as React from "react";
+import { cn } from "@/lib/utils.ts";
 
 function TooltipProvider({
 	delayDuration = 0,
@@ -53,4 +53,4 @@ function TooltipContent({
 	);
 }
 
-export { Tooltip, TooltipTrigger, TooltipContent, TooltipProvider };
+export { Tooltip, TooltipContent, TooltipProvider, TooltipTrigger };
